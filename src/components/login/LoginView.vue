37bccdccca89fe6803d5473d98a97b24060fe93c
Hiltunen Matias <matias.hiltunen@lapinamk.fi>
2022-04-24 15:41:13 +0300

    add auth1&2

---
+++ src/components/login/LoginView.vue
@@ -0,0 +1,51 @@
+<script setup>
+import { onClickOutside } from '@vueuse/core';
+import { inject, reactive, ref } from 'vue'
+
+const showLoginView = inject('showLogin')
+
+const credentials = reactive({
+    username: '',
+    password: ''
+})
+
+const target = ref(null)
+
+onClickOutside(target, ()=>{
+    showLoginView.value = false
+})
+
+</script>
+
+<template>
+    <form class="login" ref="target">
+        <label>Käyttäjänimi</label>
+        <input v-model="credentials.username" type="text">
+
+        <label>Salasana</label>
+        <input v-model="credentials.password" type="password">
+
+        <button type="submit">Kirjaudu</button>
+    </form>
+</template>
+
+<style>
+
+.login {
+    width: 250px;
+    position: fixed;
+    left: 50%;
+    right: 50%;
+    top: 50%;
+    display: flex;
+    flex-flow: column wrap;
+    background-color: aqua;
+    transform: translateX(-50%) translateY(-50%);
+    padding: 20px;
+}
+
+input, label {
+    margin-top: 6px;
+}
+
+</style>
--- src/components/navigation/NavigationBar.vue
+++ src/components/navigation/NavigationBar.vue
@@ -1,6 +1,11 @@
 <script setup>
+import { provide, ref } from 'vue';
 import { RouterLink } from 'vue-router'
+import LoginView from '../login/LoginView.vue';
 
+const showLoginView = ref(false)
+
+provide('showLogin', showLoginView)
 
 </script>
 
@@ -9,7 +14,10 @@ import { RouterLink } from 'vue-router'
         <router-link to="/">Koti</router-link>
         <router-link to="/create">Uusi</router-link>
         <router-link to="/users">Käyttäjät</router-link>
+        <a href="#" @click.prevent="showLoginView = !showLoginView">Kirjaudu</a>
     </div>
+
+    <LoginView v-if="showLoginView"></LoginView>
 </template>
 
 <style>
